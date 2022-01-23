5fc36cedc1957a75d7ee7aa176ad1ec0aff7758c
Yejin Lee <ezy@Yejins-MacBook-Pro.local>
2022-01-23 20:18:59 +0900

    port 5000

--- flask-sample/app.py
+++ flask-sample/app.py
@@ -79,4 +79,4 @@ class MoivesResource(Resource):
         return Response(status=200)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
